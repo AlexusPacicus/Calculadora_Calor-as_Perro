019daf2f0af104e0329f4a6db16f3e33d2336ea8
test <test@MacBook-Air-test.local>
2025-05-20 19:19:27 +0200

    Actualiza DANTE.py: funciones RER y calorías totales

--- DANTE.py
+++ DANTE.py
@@ -1,19 +1,43 @@
-
+##
 def introducir_datos_perro():
 
 
     
-    Nombre = input('Introducir el nombre del perro')
+    nombre = input('Introducir el nombre del perro')
 
-    Edad = int (input('Edad'))
+    edad = int (input('Edad'))
 
-    Peso = float (input('Peso en kg'))
+    peso = float (input('Peso en kg'))
 
     opciones_validas = ['Bajo', 'Medio', 'Alto']
-    Nivel_Actividad_Fisica = ''
-    while Nivel_Actividad_Fisica not in opciones_validas:
-        Nivel_Actividad_Fisica = input('Nivel de actividad física: Bajo / Medio / Alto: ').capitalize()
+    nivel_actividad_fisica = ''
+    while nivel_actividad_fisica not in opciones_validas:
+        nivel_actividad_fisica = input('Nivel de actividad física: Bajo / Medio / Alto: ').capitalize()
                                     
-        if Nivel_Actividad_Fisica not in opciones_validas:
+        if nivel_actividad_fisica not in opciones_validas:
             print('Opciones válidas: Bajo / Medio / Alto')
+    return nombre, edad, peso, nivel_actividad_fisica        
+# DEFINIMOS UNA FUNCIÓN EN LA QUE CALCULAMOS EL GASTO CALÓRICO EN REPOSO (RER)#  
+   
+def calcular_rer(peso):
+
+    rer = 70 * (peso ** 0.75)
+
+    return (rer)
+
+
+rango_actividad= {
+    'Bajo': 1.2,
+    'Medio': 1.4,
+    'Alto': 1.6
+}                                                                               
+
+def calorias_totales (peso, nivel_actividad_fisica):
     
+    rer = calcular_rer(peso)
+    niveles = rango_actividad.get(nivel_actividad_fisica, 1.0)
+
+
+    return rer * niveles
+
+if __name__ == '__main__':
